e29015feab6ac896185b06f6db68bfb2cbfe5e42
Daniel Silverman <me@agperson.com>
2013-10-11 14:46:36 -0400

    Restructuring and fixing bootstrap/Vagrantfile
    Vagrant VM successfully boots and provisions on localhost, modules
    downloaded by r10k are gitignored, moved hiera up a level, removed
    unnecessary modules and configuration

--- resources/common/puppet/manifests/init.pp
+++ resources/common/puppet/manifests/init.pp
@@ -2,10 +2,4 @@ node default {
   stage { 'pre': before => Stage['main'] }
   class { 'epel': stage => 'pre' }
   class { 'common': }
-
-  class { 'mysql': }
-  class { 'mysql::server': }
-  class { 'mysql::java': }
-  class { 'mysql::python': }
-  class { 'mysql::ruby': }
 }
--- resources/development/bootstrap.sh
+++ resources/development/bootstrap.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 [[ $(rpm -qa | grep ^git-) ]] || (yum install -y -q git && sleep 5)
 [[ "$(gem query -i -n r10k)" == "true" ]] || gem install --no-rdoc --no-ri r10k
-rsync -a /puppet ~/puppet
-cd ~/puppet && r10k -v info puppetfile install
+cd /common/puppet && r10k -v info puppetfile install
