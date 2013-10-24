991294df1d84adc87bc1fc1cc0c43671888d4881
Daniel Silverman <me@agperson.com>
2013-10-24 18:40:10 -0400

    New hook structure and cross-distro bootstrap
    Implemented new hook structure with bootstrap/configure/deploy as
    required hooks for all cloudlets.  Bootstrap script should work across
    RHEL derivitives as well as Debian/Ubuntu (tested on CentOS 6 and Ubuntu
    Precise), gets Puppet, Facter, Hiera, and dependencies running in
    preparation for configuration step.  Using Hashicorp's puppet-bootstrap
    where appropriate.

--- resources/development/bootstrap.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-[[ $(rpm -qa | grep ^git-) ]] || (yum install -y -q git && sleep 5)
-[[ "$(gem query -i -n r10k)" == "true" ]] || gem install --no-rdoc --no-ri r10k
-cd /common/puppet && r10k -v info puppetfile install
